344041cd8657f6c9aff5a9a4003d8d7872c508e1
realmojo <strikers1999@naver.com>
2025-06-28 15:44:15 +0900

    Update page.tsx

--- src/app/quiz/[type]/page.tsx
+++ src/app/quiz/[type]/page.tsx
@@ -1,12 +1,5 @@
-import QuizModalPage from "@/app/@modal/(.)quiz/[type]/QuizModalClient";
+import QuizModalPage from "@/app/@modal/(.)quiz/[type]/page";
 
-type PageProps = {
-  params: {
-    type: string;
-  };
-};
-
-export default async function QuizPage({ params }: PageProps) {
-  // 이 위치에서 params 사용은 OK (함수가 async이기 때문에)
-  return <QuizModalPage type={params.type} />;
+export default async function QuizPage({ params }: any) {
+  return <QuizModalPage params={params} />;
 }
