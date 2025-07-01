0827deba8974bf13141011001fead4a0bc056929
정만경/방송DX팀 <strikers1999@naver.com>
2025-07-01 15:01:43 +0900

    Update page.tsx

--- src/app/quiz/[type]/page.tsx
+++ src/app/quiz/[type]/page.tsx
@@ -1,17 +1,18 @@
 // app/quiz/[type]/page.tsx
+
 import { redirect } from "next/navigation";
 
-interface Props {
+export default function Page({
+  params,
+  searchParams,
+}: {
   params: { type: string };
   searchParams: { answerDate?: string };
-}
-
-export default function QuizTypePage({ params, searchParams }: Props) {
+}) {
   const { type } = params;
   const { answerDate } = searchParams;
 
   if (!answerDate) {
-    // answerDate 없으면 today 등 기본 처리 가능
     redirect(`/quiz/${type}/today`);
   }
 
