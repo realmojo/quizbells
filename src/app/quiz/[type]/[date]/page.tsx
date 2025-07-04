2b3fde16ed7b30cbde05ea1b88cdd7a4664b1a7b
realmojo <strikers1999@naver.com>
2025-07-04 22:49:38 +0900

    ㅍ

--- src/app/quiz/[type]/[date]/page.tsx
+++ src/app/quiz/[type]/[date]/page.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata({
   const typeName = item?.typeKr || type;
   const typeTitle = item?.title || "";
   const dateLabel = answerDate.replace(/-/g, "년 ").replace(/-/, "월 ") + "일";
-  const fullTitle = `${typeName} ${typeTitle} ${dateLabel === "today일" ? "오늘" : dateLabel} 퀴즈 정답 확인하고 앱테크 포인트 받으세요`;
+  const fullTitle = `${typeName} ${typeTitle} ${dateLabel === "today일" ? "오늘" : dateLabel} 퀴즈 정답 확인하고 앱테크 포인트 받으세요 | 퀴즈벨`;
   const description = `${dateLabel === "today일" ? "오늘" : dateLabel} 기준 ${typeName} 퀴즈 정답을 한 눈에 확인하고, 앱테크 리워드를 적립해 보세요. 다양한 퀴즈 이벤트가 매일 업데이트됩니다.`;
 
   return {
