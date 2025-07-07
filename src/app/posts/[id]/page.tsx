51afa0b0ec539082cba710aa9661128659af8b96
realmojo <strikers1999@naver.com>
2025-07-07 15:43:57 +0900

    Update page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,6 +1,7 @@
 import PostComponent from "@/components/PostComponent";
+import { use } from "react";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const { id } = params; // 여기서 안전하게 꺼냄
-  return <PostComponent id={id} />;
+export default function Page(props: Promise<{ params: { id: string } }>) {
+  const { params } = use(props);
+  return <PostComponent id={params.id} />;
 }
