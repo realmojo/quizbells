4ad5becdc3482058705d53d8e2367fce06d2f3a2
realmojo <strikers1999@naver.com>
2025-12-17 22:04:49 +0900

    eff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -146,6 +146,12 @@ export default async function RootLayout({
           `,
           }}
         />
+        <script
+          id="clarity-tracking"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt"; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "umxzbajlwf");`,
+          }}
+        />
         <GoogleAdSense />
         {/* 구조화된 데이터 (Schema.org JSON-LD) - Organization & WebApplication */}
         <script
