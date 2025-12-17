3b4aa2a6a9e36e1e80a9fdf53fb776ecba65497b
realmojo <strikers1999@naver.com>
2025-12-18 00:46:30 +0900

    vw

--- src/components/CoupangPartnerAd.tsx
+++ src/components/CoupangPartnerAd.tsx
@@ -1,36 +1,48 @@
 export default function CoupangPartnerAd() {
   const coupangItems = [
     {
-      url: "https://naver.me/GnRHw1eJ",
+      // url: "https://naver.me/GnRHw1eJ",
+      url: "https://link.coupang.com/a/dfopJs",
       img: "/images/cp/cp-1.png",
     },
     {
-      url: "https://naver.me/F5aYt2rN",
+      // url: "https://naver.me/F5aYt2rN",
+      url: "https://link.coupang.com/a/dfop1G",
       img: "/images/cp/cp-2.png",
     },
     {
-      url: "https://naver.me/FbqX3ydL",
+      // url: "https://naver.me/FbqX3ydL",
+      url: "https://link.coupang.com/a/dfoqgF",
       img: "/images/cp/cp-3.png",
     },
     {
-      url: "https://naver.me/FIN4DZgA",
+      // url: "https://naver.me/FIN4DZgA",
+      url: "https://link.coupang.com/a/dfoqwd",
       img: "/images/cp/cp-4.png",
     },
     {
-      url: "https://naver.me/GFCjJ8VB",
+      // url: "https://naver.me/GFCjJ8VB",
+      url: "https://link.coupang.com/a/dfoqQb",
       img: "/images/cp/cp-5.png",
     },
     {
-      url: "https://naver.me/FOZj6Bli",
+      // url: "https://naver.me/FOZj6Bli",
+      url: "https://link.coupang.com/a/dfoq6U",
       img: "/images/cp/cp-6.png",
     },
     {
-      url: "https://naver.me/xfYBqn1V",
+      // url: "https://naver.me/xfYBqn1V",
+      url: "https://link.coupang.com/a/dforjk",
       img: "/images/cp/cp-7.png",
     },
     {
-      url: "https://naver.me/FdodmcmD",
+      // url: "https://naver.me/FdodmcmD",
+      url: "https://link.coupang.com/a/dforvO",
       img: "/images/cp/cp-8.png",
+    },
+    {
+      url: "https://link.coupang.com/a/dforYi",
+      img: "/images/cp/cp-10.png",
     },
   ];
 
