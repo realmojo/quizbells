361c4ddbdba002d2368892a9ec46e677d8840024
realmojo <strikers1999@naver.com>
2025-12-18 19:12:24 +0900

    ff

---
+++ src/app/api/naver/indexnow/all/route.ts
@@ -0,0 +1,38 @@
+import { quizItems } from "@/utils/utils";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const host = "quizbells.com";
+  const key = "b21c58144b521f5656d122efdeaa208f";
+
+  const urlList: string[] = [];
+  quizItems.map((item) => {
+    urlList.push(`https://quizbells.com/quiz/${item.type}/today`);
+  });
+
+  const params = {
+    host,
+    key,
+    keyLocation: `https://${host}/${key}.txt`,
+    urlList,
+  };
+
+  // POST 요청 예시
+  const indexNowURL = "https://searchadvisor.naver.com/indexnow";
+  const response = await fetch(indexNowURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  });
+
+  if (response.ok) {
+    return NextResponse.json({ message: "IndexNow request sent successfully" });
+  } else {
+    return NextResponse.json(
+      { message: "Failed to send IndexNow request" },
+      { status: response.status }
+    );
+  }
+}
--- src/app/api/naver/indexnow/route.ts
+++ src/app/api/naver/indexnow/route.ts
@@ -1,30 +1,20 @@
-import { quizItems } from "@/utils/utils";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const host = "quizbells.com";
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const type = searchParams.get("type");
+
+  if (!type) {
+    return NextResponse.json({ error: "type is required" }, { status: 400 });
+  }
+
+  const url = `https://quizbells.com/quiz/${type}/today`;
   const key = "b21c58144b521f5656d122efdeaa208f";
 
-  const urlList: string[] = [];
-  quizItems.map((item) => {
-    urlList.push(`https://quizbells.com/quiz/${item.type}/today`);
-  });
-
-  const params = {
-    host,
-    key,
-    keyLocation: `https://${host}/${key}.txt`,
-    urlList,
-  };
-
-  // POST 요청 예시
-  const indexNowURL = "https://searchadvisor.naver.com/indexnow";
+  // GET 요청 예시
+  const indexNowURL = `https://searchadvisor.naver.com/indexnow?url=${url}&key=${key}`;
   const response = await fetch(indexNowURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
+    method: "GET",
   });
 
   if (response.ok) {
