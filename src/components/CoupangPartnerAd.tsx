fcfdea011f6a3e43aa032af7acc9674264af994d
정만경/방송DX팀 <6177160+realmojo@users.noreply.github.com>
2025-12-19 15:33:55 +0900

    ef

--- src/components/CoupangPartnerAd.tsx
+++ src/components/CoupangPartnerAd.tsx
@@ -86,7 +86,7 @@ export default function CoupangPartnerAd() {
   return (
     <iframe
       src="https://ads-partners.coupang.com/widgets.html?id=952091&template=carousel&trackingCode=AF5242985&subId=&width=380&height=380&tsource="
-      style={{ width: "100%", height: "380px" }}
+      style={{ width: "100%", height: "200px" }}
       frameBorder="0"
       scrolling="no"
       referrerPolicy="unsafe-url"
