e8bef47f0f698d96749c5eaf5ed1b86efd83b2fb
realmojo <strikers1999@naver.com>
2025-12-24 22:38:31 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  htmlLimitedBots: /.*/,
+  // htmlLimitedBots: /.*/,
   // Cloudflare Pages 호환성 설정
   // Cloudflare Pages는 Next.js를 자동으로 감지하고 최적화합니다
   images: {
