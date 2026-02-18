03fa54f2564fcb44df6001df94e2fe2d2f244117
realmojo <strikers1999@naver.com>
2026-02-18 15:00:12 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -170,7 +170,7 @@ export default async function RootLayout({
               window.googletag = window.googletag || {cmd: []};
               window.googletag.cmd = window.googletag.cmd || [];
               googletag.cmd.push(function() {
-                googletag.defineSlot('/23331430035/quizbells_main_top', [[336, 280], 'fluid'], 'div-gpt-ad-1771389317304-0').addService(googletag.pubads());
+                googletag.defineSlot('/23331430035/quizbells_main_top', [[336, 280], [250, 250], [750, 200], [970, 250], 'fluid', [750, 300]], 'div-gpt-ad-1771394382291-0').addService(googletag.pubads());
                 googletag.pubads().enableSingleRequest();
                 googletag.enableServices();
               });
